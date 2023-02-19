09ccf3c277044cbc3b6ab769b48aaf39c7111b75
MinhVuongTran <minhvuongtran9x@gmail.com>
2023-02-19 14:18:29 +0700

    fix request

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const APP_ID = "16c8f05ef8b52bcbbde2111462fabce5";
+const APP_ID = '16c8f05ef8b52bcbbde2111462fabce5';
 
-const searchInput = document.querySelector("#search-input");
-const cityName = document.querySelector(".city-name");
-const weatherState = document.querySelector(".weather-state");
-const weatherIcon = document.querySelector(".weather-icon");
-const temperature = document.querySelector(".temperature");
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherState = document.querySelector('.weather-state');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
 
-const sunrise = document.querySelector(".sunrise");
-const sundown = document.querySelector(".sundown");
-const humidity = document.querySelector(".humidity");
-const windSpeed = document.querySelector(".wind-speed");
+const sunrise = document.querySelector('.sunrise');
+const sundown = document.querySelector('.sundown');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
 
-searchInput.addEventListener("change", (e) => {
-  fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${e.target.value}&limit=5&appid=${APP_ID}`
-  ).then(async (res) => {
-    const data = await res.json();
-    const lat = data[0].lat;
-    const lon = data[0].lon;
+searchInput.addEventListener('change', (e) => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_ID}&units=metric&lang=vi`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${e.target.value}&limit=5&appid=${APP_ID}`,
     ).then(async (res) => {
-      const data = await res.json();
-      console.log(data);
-      cityName.innerHTML = data.name;
-      weatherState.innerHTML = data.weather[0].description;
-      weatherIcon.setAttribute(
-        "src",
-        `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-      );
-      temperature.innerHTML = Math.round(data.main.temp);
-      sunrise.innerHTML = moment.unix(data.sys.sunrise).format("H:mm");
-      sundown.innerHTML = moment.unix(data.sys.sunset).format("H:mm");
-      humidity.innerHTML = data.main.humidity;
-      windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2);
+        const data = await res.json();
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APP_ID}&units=metric&lang=vi`,
+        ).then(async (res) => {
+            const data = await res.json();
+            console.log(data);
+            cityName.innerHTML = data.name;
+            weatherState.innerHTML = data.weather[0].description;
+            weatherIcon.setAttribute(
+                'src',
+                `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+            );
+            temperature.innerHTML = Math.round(data.main.temp);
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm');
+            sundown.innerHTML = moment.unix(data.sys.sunset).format('H:mm');
+            humidity.innerHTML = data.main.humidity;
+            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2);
+        });
     });
-  });
 });
